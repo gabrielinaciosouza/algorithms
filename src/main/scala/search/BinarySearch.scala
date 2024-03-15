26d581e77fd7c9d7bd9c511fe1e5b7c448acacb6
gabrielinaciosouza <gabriel.sert9@gmail.com>
2024-03-15 22:59:17 +0100

    making BinarySearch immutable

--- src/main/scala/search/BinarySearch.scala
+++ src/main/scala/search/BinarySearch.scala
@@ -1,25 +1,31 @@
 package search
 
+import scala.annotation.tailrec
+
 case object BinarySearch {
 
   def apply[A](iterable: Iterable[A], element: A)(implicit ordering: Ordering[A]): Option[A] = {
     val seq = iterable.toSeq
-    var low = 0
-    var high = seq.length - 1
 
-    while (low <= high) {
-      val mid = (low + high) / 2
-      val value = seq(mid)
+    @tailrec
+    def helper(low: Int, high: Int, result: Option[A]): Option[A] = {
+      if (low > high) {
+        result
+      } else {
+        val mid = (low + high) / 2
+        val value = seq(mid)
 
-      val comp = ordering.compare(value, element)
+        val comp = ordering.compare(value, element)
 
-      comp match {
-        case x if x < 0 => low = mid + 1
-        case x if x > 0 => high = mid - 1
-        case _ => return Some(value)
+        comp match {
+          case x if x < 0 => helper(mid + 1, high, result)
+          case x if x > 0 => helper(low, mid - 1, result)
+          case _ => Some(value)
+        }
       }
     }
-    None
+
+    helper(0, seq.length - 1, None)
   }
 
 }
